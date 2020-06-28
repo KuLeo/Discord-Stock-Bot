dc9d8c097c403c2e387c50bac33ef0fef281a2cd
KuLeo <ms0371096@gmail.com>
2020-06-28 12:11:18 +0800

    Add bot sent message to channel function

--- Bot.py
+++ Bot.py
@@ -2,4 +2,24 @@ import discord
 from discord.ext import commands
 
 
-bot = commands.Bot()
+bot = commands.Bot(command_prefix='.')
+
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(726649005366575135)
+    await channel.send('>> Bot is Online <<')
+    print(">> Bot is Online")
+
+
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join!")
+
+
+@bot.event
+async def on_member_remove(member):
+    print(f"{member} leave!")
+
+
+bot.run('NzI2MzM3MzAzNDEyMzQyODI0.XvgFRg.QfZPXdWZF5VgKNe1_9_3wJll_ms')
