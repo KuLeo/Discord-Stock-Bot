da9ece10a80b2c337026fb7ffaea942c0364710b
KuLeo <ms0371096@gmail.com>
2020-06-28 12:42:25 +0800

    Add Send Picture function

--- Bot.py
+++ Bot.py
@@ -1,4 +1,11 @@
+import json
+
+import discord
 from discord.ext import commands
+
+
+with open('setting.json', 'r', encoding='utf-8') as setting:
+    jdata = json.load(setting)
 
 
 bot = commands.Bot(command_prefix='.')
@@ -6,7 +13,7 @@ bot = commands.Bot(command_prefix='.')
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(726649005366575135)
+    channel = bot.get_channel(int(jdata['MAIN_CHANNEL']))
     await channel.send('>> Bot is Online <<')
     print(">> Bot is Online")
 
@@ -25,4 +32,11 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency * 1000, 2)} (ms)')
 
-bot.run('NzI2MzM3MzAzNDEyMzQyODI0.XvgFRg.QfZPXdWZF5VgKNe1_9_3wJll_ms')
+
+@bot.command()
+async def pic(ctx):
+    pic_file = discord.File('E:\\20200516_102659.jpg')
+    await ctx.send(file=pic_file)
+
+
+bot.run(jdata['TOKEN'])
